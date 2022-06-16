f805b8c3c87e2b90d87104dafdbadceee30376e5
Selinda-C <selindachapman@icloud.com>
2022-06-16 15:52:19 -0700

    add reset button

--- js/script.js
+++ js/script.js
@@ -1,30 +1,50 @@
 function hideResults(){
-  let cSharp = document.getElementById("cSharp")
-  let javaScript = document.getElementById("javaScript")
-  let python = document.getElementById("python")
+  let cSharp = document.getElementById("cSharp");
+  let javaScript = document.getElementById("javaScript");
+  let python = document.getElementById("python");
+  let reset = document.getElementById("reset");
+  reset.style.display= "none";
   cSharp.style.display = "none";
   javaScript.style.display = "none";
   python.style.display = "none";
 }
 window.addEventListener("load", function(){
   hideResults();
-  let form = document.querySelector("form")
+  let form = document.querySelector("form");
   form.addEventListener("submit", function(event){
     event.preventDefault();
     const food = document.querySelector("input[name='food']:checked").value;
-    if (food === "sushi") {       
-      python.removeAttribute("style") 
+    const age = document.querySelector("input#age").value;
+    if (food === "sushi" && age < 20) {       
+      python.removeAttribute("style");
       cSharp.style.display = "none";
       javaScript.style.display = "none";
-    } else if (food === "chocolate") {
-      javaScript.removeAttribute("style")
+    } else if (age >= 20 || food === "chocolate") {
+      javaScript.removeAttribute("style");
       cSharp.style.display = "none";
       python.style.display = "none";
     } else {
-      cSharp.removeAttribute("style")
+      cSharp.removeAttribute("style");
       python.style.display = "none";
       javaScript.style.display = "none";
     };
     event.preventDefault();
+    reset.removeAttribute("style");
+  });
+  let color = document.getElementById("color");
+  let food = document.getElementsByName("food");
+  let age = document.getElementById("age");
+  let pets = document.getElementById("pets");
+  let hobby = document.getElementById("hobby");
+  resetBtn.addEventListener("click", function(){
+    reset.style.display= "none";
+    cSharp.style.display = "none";
+    javaScript.style.display = "none";
+    python.style.display = "none";
+    color.value = null;
+    food.value = null;
+    age.value = null;
+    pets.value = null;
+    hobby.value = null;
   });
 });
